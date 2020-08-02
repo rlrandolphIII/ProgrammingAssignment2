5d9f3e51a1cb8eb2c924a7db3e74d2e5363fffa1
Richard L. Randolph, III <rlrandolphIII@gmail.com>
2020-08-01 20:37:33 -0700

    my third commit of cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -40,27 +40,3 @@ cacheSolve <- function(x, ...) {
         x$setinv(inv)
         inv
 }
-
-test = function(mat){
-        ## @mat: an invertible matrix
-        
-        temp = makeCacheMatrix(mat)
-        
-        start.time = Sys.time()
-        cacheSolve(temp)
-        dur = Sys.time() - start.time
-        print(dur)
-        
-        start.time = Sys.time()
-        cacheSolve(temp)
-        dur = Sys.time() - start.time
-        print(dur)
-}
-
-set.seed(1110201)
-r = rnorm(1000000)
-mat1 = matrix(r, nrow=1000, ncol=1000)
-test(mat1)
-
-test10 <- makeCacheMatrix(mat1)
-cacheSolve(test10)
